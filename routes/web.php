5242261757ace99bf741356551eb8c42ec588f06
Greg Ulrich <gregu@24data.com>
2020-03-31 16:48:07 -0400

    Do delete

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,44 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+// Raw SQL query format
+Route::get('/insert', function() {
+   DB::insert('insert into posts(title, body) values(?,?)', ['New Title', 'HERE is some new text']);
+
+});
+
+
+// Get info from database
+Route::get('/read', function() {
+
+$results = DB::select('select * from posts');
+$print = '';
+foreach($results as $result) {
+    $print = $print. $result->title . "<br>";
+}
+return $print;
+});
+
+Route::get('/update', function() {
+    $updated = DB::update('update posts set title = "Updated Title" where id = ?', [1]);
+    return "Post Title has been updated " . $updated ;
+});
+
+Route::get('/destroy', function() {
+
+    $result = DB::delete('delete from posts where id = ?', [4]);
+    if ($result === 1) {
+        return "Updated";
+    } else {
+        return "error";
+    }
+
+
+});
+
+
+
 // Route::get('/{id}', function($id) {
 //     return "This is the id: " . $id;
 // });
