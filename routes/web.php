4ce77a87b927c5a764ac971e942bce9d3b4dfa08
Greg Ulrich <gregu@24data.com>
2020-04-20 09:16:30 -0400

    Dtatbase

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,51 +17,93 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+// ****************************************************************************
 // Raw SQL query format
-Route::get('/insert', function() {
-   DB::insert('insert into posts(title, body) values(?,?)', ['New Title', 'HERE is some new text']);
-
-});
-
-
-// Get info from database
+// ****************************************************************************
 Route::get('/read', function() {
-
-$results = DB::select('select * from posts');
-$print = '';
-foreach($results as $result) {
-    $print = $print. $result->title . "<br>";
-}
-return $print;
-});
-
-Route::get('/update', function() {
-    $updated = DB::update('update posts set title = "Updated Title" where id = ?', [1]);
-    return "Post Title has been updated " . $updated ;
-});
-
-Route::get('/destroy', function() {
-
-    $result = DB::delete('delete from posts where id = ?', [4]);
-    if ($result === 1) {
-        return "Updated";
-    } else {
-        return "error";
-    }
-
-
+    $results = DB::select('SELECT * FROM posts ');
+    return var_dump($results);
+    // foreach($results as $result) {
+    //     return $result->title;
+    // }
 });
 
 
+// // Get info from database
+// Route::get('/read', function() {
 
-// Route::get('/{id}', function($id) {
-//     return "This is the id: " . $id;
+// $results = DB::select('select * from posts');
+// $print = '';
+// foreach($results as $result) {
+//     $print = $print. $result->title . "<br>";
+// }
+// return $print;
 // });
 
-// Route::get('/post/{data}', 'PostController@index');
-// Route::resource('posts', 'PostController');
+// Route::get('/update', function() {
+//     $updated = DB::update('update posts set title = "Updated Title" where id = ?', [1]);
+//     return "Post Title has been updated " . $updated ;
+// });
 
-Route::get('/contact', 'PostController@contact');
+// Route::get('/destroy', function() {
 
-Route::get('post/{id}/{name}/{login}', 'PostController@show_post');
+//     $result = DB::delete('delete from posts where id = ?', [4]);
+//     if ($result === 1) {
+//         return "Updated";
+//     } else {
+//         return "error";
+//     }
+
+
+// });
+
+
+
+// // Route::get('/{id}', function($id) {
+// //     return "This is the id: " . $id;
+// // });
+
+// // Route::get('/post/{data}', 'PostController@index');
+// // Route::resource('posts', 'PostController');
+
+// Route::get('/contact', 'PostController@contact');
+
+// Route::get('post/{id}/{name}/{login}', 'PostController@show_post');
+
+
+
+
+
+// ****************************************************************************
+// Eloquent 
+// ****************************************************************************
+
+// Route::get('/read', function() {
+// // Pulls all the methods from Posts controller
+//     $posts = Post::all();
+//     // $collection = [];
+//     // foreach($posts as $post) {
+//     //     array_push($post->title, $collection);
+//     // }
+//     // $count = count($collection);
+
+//     return $posts;
+
+// });
+
+// Route::get('/find', function() {
+
+//     $posts = Post::find(2);
+
+//     return $posts->body;
+// });
+
+// Route::get('/findwhere', function() {
+//     $posts = Post::where('id', 2)->get();
+//     return $posts;
+// });
+
+// Route::get('/findmore', function() {
+//     $posts = Post::findorfail(5);
+//     return $posts->title;
+// });
